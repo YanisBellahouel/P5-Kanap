5748bb01717e565f8f002058c555e97d4b326d59
yanisbellahouel <yanis.bellahouel76@gmail.com>
2022-09-24 14:03:21 +0200

    t

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,9 +14,34 @@ function basket (){
 
 }
 
+let article = document.createElement("article");
+
 let newDiv = document.createElement("div");
 let img = document.createElement("img");
 
 let newDiv1 = document.createElement("div");
-let newDiv2 = document.createElement("div");
+let description = document.createElement("div");
+let title = document.createElement("h2");
+let color = document.createElement("p");
+let price = document.createElement("p");
 
+document.getElementById("cart__items").appendChild(article);
+
+article.appendChild(newDiv);
+article.appendChild(newDiv1);
+
+newDiv.appendChild(img);
+
+newDiv1.appendChild(description);
+
+description.appendChild(title);
+description.appendChild(color);
+description.appendChild(price);
+
+article.setAttribute("panier", [basket]);
+
+
+
+title.textContent = Selection.name;
+color.textContent = Selection.color;
+price.textContent = Selection.price;
